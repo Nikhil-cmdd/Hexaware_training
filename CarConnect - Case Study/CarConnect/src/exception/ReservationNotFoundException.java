9340a4c549423891cfabe285c8a936dfb4424769
Nikhil Singh <singhnikhil.connect@gmail.com>
2025-04-11 01:26:58 +0530

    Delete CarConnect - Case Study/CarConnect directory

--- CarConnect - Case Study/CarConnect/src/exception/ReservationNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class ReservationNotFoundException extends RuntimeException {
-	public static final long serialVersionUID =  1L;
-	
-    public ReservationNotFoundException(String message) {
-        super(message);
-    }
-}
--- CarConnect - Case Study/CarConnect/src/exception/VehicleNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class VehicleNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-	
-    public VehicleNotFoundException(String message) {
-        super(message);
-    }
-}
