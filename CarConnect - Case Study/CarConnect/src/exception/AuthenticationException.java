c755e4d8f5ceda9553c16cd80f899d9117769cc7
Nikhil Singh <singhnikhil.connect@gmail.com>
2025-04-11 16:13:35 +0530

    Delete CarConnect - Case Study/CarConnect directory

--- CarConnect - Case Study/CarConnect/src/exception/AuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class AuthenticationException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
--- CarConnect - Case Study/CarConnect/src/exception/CustomerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class CustomerNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-	
-    public CustomerNotFoundException(String message) {
-        super(message);
-    }
-}
